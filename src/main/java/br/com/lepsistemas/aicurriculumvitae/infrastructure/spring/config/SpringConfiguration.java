640fe342355701bc6c59128c2599b4c5eeda21fd
Leandro Boeing Vieira <leandro.vieira@softplan.com.br>
2018-12-17 10:51:04 -0200

    Adding Cors

--- src/main/java/br/com/lepsistemas/aicurriculumvitae/infrastructure/spring/config/SpringConfiguration.java
+++ src/main/java/br/com/lepsistemas/aicurriculumvitae/infrastructure/spring/config/SpringConfiguration.java
@@ -1,4 +1,4 @@
-package br.com.lepsistemas.aicurriculumvitae.infrastructure.spring;
+package br.com.lepsistemas.aicurriculumvitae.infrastructure.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
