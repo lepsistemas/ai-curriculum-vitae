2bf0ee8a2052b7f109accc1e8983097c951bf396
Leandro Boeing Vieira <leandro.vieira@softplan.com.br>
2019-02-10 23:37:59 -0200

    Removing tap right button text

--- src/main/java/br/com/lepsistemas/aicurriculumvitae/domain/Resume.java
+++ src/main/java/br/com/lepsistemas/aicurriculumvitae/domain/Resume.java
@@ -41,7 +41,7 @@ public class Resume implements Domain {
 
 	@Override
 	public String toString() {
-		return "Leandro's curriculum is vast. I think you'll get tired of listening entirely. Please, tap on the right button and I'll take you to his Curriculum Vitae page. From there, you'll be able to send it to your e-mail.";
+		return "Leandro's curriculum is vast. I think you'll get tired of listening entirely.";
 	}
 
 }
